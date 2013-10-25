1cf2d8a563b26c00d1b1a083149deca76563963e
Tomokazu Hirai <tomokazu.hirai@kddi-web.com>
2013-10-25 10:20:39 +0900

    changed to modify heat.conf

--- heat.sh
+++ heat.sh
@@ -4,7 +4,7 @@ function heat_setup() {
   # install packages
   install_package heat-api heat-api-cfn
 
-  # create database for cinder
+  # create database for heat
   mysql -uroot -p${mysql_pass} -e "create database heat;"
   mysql -uroot -p${mysql_pass} -e "grant all on heat.* to '${db_heat_user}'@'%' identified by '${db_heat_pass}';"
 
@@ -14,6 +14,11 @@ function heat_setup() {
     "<keystone_ip>:${keystone_ip}" \
     "<service_tenant_name>:${service_tenant_name}" \
     "<service_password>:${service_password}"
+
+  setconf infile:$base_dir/conf/etc.heat/heat.conf \
+    outfile:/etc/heat/heat.conf \
+    "<db_ip>:${db_ip}" "<db_heat_user>:${db_heat_user}" \
+    "<db_heat_pass>:${db_heat_pass}"
 
   # input database for heat
   heat-manage db_sync
